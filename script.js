5dbfd0731928da1edc97d46d5bd3f085fe0a119a
Hamad Zia <hamadzia74@gmail.com>
2024-07-20 18:16:46 +0500

    Implemented Chaining Promises

--- script.js
+++ script.js
@@ -100,6 +100,13 @@ getCountryAndNeighbour('pakistan');
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+      if (!neighbour) return;
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
-getCountryData('portugal');
+getCountryData('germany');
